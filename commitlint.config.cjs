b132d332282714f175a4b5cf47b09a5f2e969ec7
hyunjiwon <jw_0819@naver.com>
2025-03-18 19:03:12 +0900

    Fix: v1.1.2

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,35 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [
-      2,
-      'always',
-      // NOTE: Common
-      // [
-      //   '✨ Feat',
-      //   'Feat',
-      //   '🐛 Fix',
-      //   'Fix',
-      //   '📝 Docs',
-      //   'Docs',
-      //   '🎨 Style',
-      //   'Style',
-      //   '♻️ Refactor',
-      //   'Refactor',
-      //   '⚡ Perf',
-      //   'Perf',
-      //   '✅ Test',
-      //   'Test',
-      //   '🔧 Chore',
-      //   'Chore',
-      //   '🔀 Merge',
-      //   'Merge',
-      //   '🚀 Release',
-      //   'Release',
-      // ],
-      // NOTE: Version
-      ['Break', 'Feat', 'Fix'],
-    ],
+    'type-enum': [2, 'always', ['Break', 'Feat', 'Fix']],
     'type-case': [0],
     'subject-case': [0],
     'header-max-length': [2, 'always', 100],
